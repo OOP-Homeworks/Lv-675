a585418fd9770737800682c49dee83968c8cc265
Luk0sS <98831511+Luk0sS@users.noreply.github.com>
2022-02-17 14:04:37 +0200

    Add files via upload

---
+++ Salnikov_HW/Salnikov_HW5/Builder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salnikov_HW5_1
+{
+    class Builder : IDeveloper, IComparable<IDeveloper>
+    {
+        public Builder(string tool)
+        {
+            this.tool = tool;
+        }
+        private string tool;
+        public string Tool
+        {
+            get { return tool; }
+            set { tool = value; }
+        }
+
+        public void Create()
+        {
+            Console.WriteLine($"\nNew builder with {Tool} created ");
+        }
+
+        public void Destroy()
+        {
+            Console.WriteLine($"\nThe builder with {Tool} destroyed ");
+        }
+
+        int IComparable<IDeveloper>.CompareTo(IDeveloper other)
+        {
+            return String.Compare(this.Tool, other.Tool);
+        }
+
+    }
+}
---
+++ Salnikov_HW/Salnikov_HW5/Programmer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salnikov_HW5_1
+{
+    class Programmer : IDeveloper, IComparable<IDeveloper>
+    {
+        public Programmer(string language)
+        {
+            this.language = language;
+        }
+        private string language;
+
+        public string Tool
+        {
+            get { return language; }
+            set { language = value; }
+        }
+
+        public void Create()
+        {
+            Console.WriteLine("\nA new programmer has been created who is " +
+                "proficient in the programming language " + Tool);
+        }
+
+        public void Destroy()
+        {
+            Console.WriteLine("\nThe " + Tool + " programmer is removed ");
+        }
+
+        int IComparable<IDeveloper>.CompareTo(IDeveloper other)
+        {
+            return String.Compare(this.Tool, other.Tool);
+        }
+    }
+}
