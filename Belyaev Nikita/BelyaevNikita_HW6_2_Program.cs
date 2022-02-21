b6ece235cf28e6da5cfa6c1e4250305e01d6d226
Nikita <doofeee@gmail.com>
2022-02-21 15:20:31 +0200

    added HW6_2

---
+++ Belyaev Nikita/BelyaevNikita_HW6_2_Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace HomeWork6_2
+{
+    class Program
+    {
+        static int number;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Write number start of range : ");
+            int start = int.Parse(Console.ReadLine());
+            Console.Write("Write number end of range : ");
+            int end = int.Parse(Console.ReadLine());
+
+            try
+            {
+                ReadNumber(start, end);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error : {ex.Message}");
+            }
+        }
+
+        public static void ReadNumber(int start, int end)
+        {
+            bool success = true;
+
+            while (success)
+            {
+                Console.Write($"Write number to check if its in range : ");
+
+                success = int.TryParse(Console.ReadLine(), out number);
+                if (!success)
+                {
+                    throw new Exception("Your number is not INT.");
+                }
+
+                if (number >= start && number <= end)
+                {
+                    Console.WriteLine($"Your number {number} in right range.");
+                }
+                else
+                {
+                    throw new Exception("Your number not in range");
+                }
+            }
+        }
+    }
+}
