d3e95db1cd5cd8af59a2674e0c795f96d5ef3494
Yan <37808891+JanHapy@users.noreply.github.com>
2022-03-19 12:26:29 +0200

    Update Square.cs

--- HomeWork8/Square.cs
+++ HomeWork8/Square.cs
@@ -9,37 +9,29 @@ namespace HomeWork8
     public class Square : Shape
     {
         private double side;
-
+        
         public double Side { get { return side; } }
-
+        
         public Square(string name, double side) : base(name)
         {
             this.side = side;
         }
 
-        public override double Area(double side)
+        public override double Area()
         {
             return side * side;
         }
-
-        public override double Perimeter(double side)
+        public override double Perimeter()
         {
             return 2 * (side + side);
         }
-
         public override string ToString()
         {
             return "Name is " + Name + ", side is " + side;
         }
-
         public override int CompareTo(object obj)
         {
-            Shape other = (Shape)obj;
-            if (other is Square)
-            {
-                return this.Perimeter(side).CompareTo(other.Perimeter((other as Square).Side));
-            }
-            return this.Perimeter(side).CompareTo(other.Perimeter((other as Circle).Radius));
+            return this.Perimeter().CompareTo((obj as Shape).Perimeter());
         }
     }
 }
