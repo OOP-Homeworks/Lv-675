75591e5698dd234dbc9e14f2611bbddcd5fa6d83
Yan <37808891+JanHapy@users.noreply.github.com>
2022-03-19 12:27:01 +0200

    Update Shape.cs

--- HomeWork8/Shape.cs
+++ HomeWork8/Shape.cs
@@ -9,25 +9,17 @@ namespace HomeWork8
     public abstract class Shape : IComparable
     {
         private string name;
-        public string Name { get { return name; } }
-
-        public Shape (string name)
+        
+        public string Name { get { return name; } set { name = value; }  }
+        
+        public Shape(string name)
         {
             this.name = name;
         }
 
-        public abstract double Area(double x);
-        public abstract double Perimeter(double x);
-        public abstract int CompareTo(object obj);
-
-        //public virtual int CompareTo(object obj1)
-        //{
-        //    Shape p1 = obj1 as Shape;
-        //    if (p1 == null)
-        //        throw new ArgumentException("Object is not a Person");
-        //    else
-        //        return this.Name.ToLower().CompareTo(p1.Name.ToLower());
-        //}
+        public abstract double Area();
+        public abstract double Perimeter();
+        public abstract int CompareTo(object obj); 
 
     }
 }
