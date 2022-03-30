2f3e09f601fe5b5a28a1d706faecc34327c8acef
DeusIDK <89904172+DeusIDK@users.noreply.github.com>
2022-03-31 01:39:17 +0300

    HW9

---
+++ 1/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW9_1
+{
+    public abstract class Shape : IComparable
+    {
+        private string name;
+        public string Name { get { return name; } set { name = value; } }
+
+        public Shape (string name)
+        {
+            this.name = name;
+        }
+
+        public abstract double Area();
+        public abstract double Perimeter();
+
+        public abstract int CompareTo(object obj);
+    }
+}
