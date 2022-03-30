34231a0cc28c960eed0a17d9578793aa617976bc
Yan <37808891+JanHapy@users.noreply.github.com>
2022-03-31 01:20:42 +0300

    Add files via upload

---
+++ HomeWork11/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp1
+{
+    public delegate void MyDel(int m);
+
+    public class Student
+    {
+        private string name;
+        public string Name { get { return "Yan"; } }
+
+        public event MyDel MarkChange;
+
+        List<int> marks = new List<int>();
+
+        public void AddMark(int m)
+        {
+            marks.Add(m);
+            if (MarkChange != null)
+                    MarkChange.Invoke(m);
+        }      
+    }
+
+    public class Parent : Student
+    {
+        public void OnMarkChange(int est)
+        {
+            Console.WriteLine($"Your son {Name} has {est} mark");
+        }
+    }
+
+    public class Accountancy : Student
+    {
+        public void PayingFellowship(int mark)
+        {
+            if (mark >= 10) 
+                Console.WriteLine($"{Name} will have stepuhy\n");
+            else
+            Console.WriteLine($"{Name} will not have stepuhy((, please study hard\n");
+        }
+    }
+    
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Student student = new Student();
+            Parent parent = new Parent();
+            Accountancy accountancy = new Accountancy();
+
+            MyDel father = parent.OnMarkChange;
+            MyDel stepuha = accountancy.PayingFellowship;
+
+            student.MarkChange += father;
+            student.MarkChange += stepuha;
+
+            student.AddMark(4);
+            student.AddMark(10);
+            student.AddMark(5);
+            student.AddMark(4);
+            student.AddMark(7);         
+
+            Console.WriteLine();
+        }
+    }
+}
